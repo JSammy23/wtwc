1cfd0f0a1721d7286ffb0839a126861695894f3d
Jordan <jordansampson23@gmail.com>
2023-03-28 12:42:03 -0500

    Initialize project

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div className="logo">Wade Through Water</div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
